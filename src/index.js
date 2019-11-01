159651ae9a03d5302db5f87892c077e9f8c65875
vermad843 <vishnuverma7524@gmail.com>
2019-11-01 10:07:10 +0530

    hello world another way

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 
 
 
 ReactDOM.render(
-  <App/>
+  <h1>hello, world!</h1>
     , document.getElementById("root"))
