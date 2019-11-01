73d51eee43453e2a44067fb4c72a65680738bc7a
vermad843 <vishnuverma7524@gmail.com>
2019-11-01 22:15:51 +0530

    functional component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//functional component : 
 
-
-const user = {
-  firstName : 'Vishnu',
-  lastName : 'Verma'
-};
-
-let fullName = function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>
 }
-
-const element = (
-  <h1>
-    Hello, {fullName(user)}!
-  </h1>
-);
-
-
-
-
+const element = <Welcome name = "Vishnu"/>
 ReactDOM.render(
    element
     , document.getElementById("root"));
